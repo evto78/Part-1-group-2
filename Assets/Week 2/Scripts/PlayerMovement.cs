38f2655da159d7ccdca0c8317fd4ad9e8d3836c6
vandyeva <vandyeva@OA-S141-26>
2024-01-17 13:59:51 -0500

    Player is moving on keyboard input

---
+++ Assets/Week 2/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Vector2 direction;
+    public Rigidbody2D rb;
+    public float force = 72.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(direction * force * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+}
