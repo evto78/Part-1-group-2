cca6b84a61488650dc3fae6da4a3e4bf21282ba2
evto78 <devesto78@gmail.com>
2024-01-29 21:57:16 -0500

    Enemy has now been added as a prefab and now progresses left over time.

--- Assets/Assignment/Scripts/Enemy.cs
+++ Assets/Assignment/Scripts/Enemy.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float speed = 1f;
+
     void Update()
     {
-        
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
